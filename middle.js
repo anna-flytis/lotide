87c79535166216f5db90d2f8f763cb4bb84d6df8
Anna Mariakhina <77463611+anna-flytis@users.noreply.github.com>
2023-04-05 15:05:41 -0600

    middle

---
+++ middle.js
@@ -0,0 +1,7 @@
+const assertEqual = function(actual, expected) {
+  if (expected === actual){
+    console.log("✅✅✅ " + "Assertion Passed: " + actual + " === " + expected);
+  } else console.log("🛑🛑🛑 " + "Assertion Failed: " + actual + " != " + expected);
+  }
+
+  
