2e9da3b893a31e1aeaabc0d825ba10754c938582
Anna Mariakhina <77463611+anna-flytis@users.noreply.github.com>
2023-04-05 21:49:45 -0600

    Add files via upload

--- middle.js
+++ middle.js
@@ -4,4 +4,20 @@ const assertEqual = function(actual, expected) {
   } else console.log("🛑🛑🛑 " + "Assertion Failed: " + actual + " != " + expected);
   }
 
+
+  const middle = function(array) {
+    if (array.length < 3) {
+      return [];
+    } else if (array.length % 2 === 0) {
+      return array.slice(array.length / 2 - 1, array.length / 2 + 1)
+    } else {
+       let newArray = [];
+       let middleNum = Math.round(array.length /2)
+       newArray.push(array[middleNum -1]);
+       return newArray;
+    }
+  };
+  
+  //module.exports = middle;
+
   
